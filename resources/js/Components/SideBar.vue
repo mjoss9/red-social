07276d00ca4f025a3eb972bdabafbabadc8eb4ff
Maria Jose <mariajosehuizacapo@gmail.com>
2022-09-12 12:50:10 -0400

    Update sidebar links and using FontAwesome icons

--- resources/js/Components/SideBar.vue
+++ resources/js/Components/SideBar.vue
@@ -1,11 +1,27 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
+// import Icon from './Icon.vue';
+
+</script>
+<script>
+export default {
+    computed: {
+        sideBarLinks() {
+            return [
+                { name: `${this.$page.props.user.username}`, image: `${this.$page.props.user.profile_photo_url}`, alt: `${this.$page.props.user.username}`, route: route('profiles.show', `${this.$page.props.user.username}`)},
+                { name: 'Edit Profile', route: route('profile.show'), icon: 'user-edit'},
+                { name: 'Members', route: route('members.index'), icon: 'users'},
+            ]
+        }
+    }
+}
 </script>
 <template>
     <aside class="flex flex-col bg-white shadow py-6 w-full sm:w-1/3 lg:w-1/4">
-        <Link :href="route('profiles.show', $page.props.user.username)" class="flex items-end pl-6 py-2 text-gray-800 hover:bg-gray-500 hover:text-white">
-            <img :src="$page.props.user.profile_photo_url" :alt="$page.props.user.username" class="h-10 w-10 rounded-full object-cover"/>
-            <span class="ml-2">{{ $page.props.user.username }}</span>
+        <Link :href="link.route" v-for="link in sideBarLinks" :key="link.name" class="flex items-end pl-6 py-2" :class="route().current(link.route) ? 'text-white bg-gray-800 hover:bg-gray-500' : 'text-gray-800 hover:bg-gray-500 hover:text-blue-500'">
+            <img :src="link.image" :alt="link.alt" class="h-8 w-8 rounded-full object-cover" v-if="link.image"/>
+            <icon :name="link.icon" class="fill-current w-8 h-8" v-if="link.icon"></icon>
+            <span class="ml-2">{{ link.name }}</span>
         </Link>
     </aside>
 </template>
